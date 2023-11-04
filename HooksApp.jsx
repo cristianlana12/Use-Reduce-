9a040130c92a6441528f77fab5c25a1545265cec
cristianlana12 <148492938+cristianlana12@users.noreply.github.com>
2023-11-04 12:18:29 -0300

    Add files via upload
    And here are the rest of the files in the src folder.

---
+++ HooksApp.jsx
@@ -0,0 +1,12 @@
+import { ListaTareas } from "./component/ListaTareas"
+
+
+export const HooksApp = () => {
+  return (
+        <>
+            <h1>Aplicacion de Hooks</h1>
+            <hr />
+            <ListaTareas/>
+        </>
+    )
+}
